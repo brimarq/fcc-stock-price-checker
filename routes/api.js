a46e734ee0a423fbe193ed15b8d0d20379c90d06
Brian Marquis <rbmarquis.dev@gmail.com>
2019-03-02 20:30:17 -0600

    correct mongodb require and add updated connect method

--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,24 @@
 */
 
 'use strict';
+require('dotenv').config(); // comment-out on Glitch
+const expect = require('chai').expect;
+const mongo = require('mongodb').MongoClient;
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+//const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
+  mongo.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
+    let db = client.db();
+    err ? console.log('Database error: ' + err) : console.log('Successful database connection');
+    
+
+    app.route('/api/stock-prices')
+      .get(function (req, res){
+        
+      });
+
+  });
     
 };
