47ced02a152c6f6ec5449d6e8d7d3b62201a08d6
Brian Marquis <rbmarquis.dev@gmail.com>
2019-03-02 19:53:50 -0600

    comment contents as these are not required

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,7 +4,7 @@
 *       FILL IN EACH UNIT TEST BELOW COMPLETELY
 *       -----[Keep the tests in the same order!]----
 *       (if additional are added, keep them at the very end!)
-*/
+
 
 var chai = require('chai');
 var StockHandler = require('../controllers/stockHandler.js');
@@ -16,3 +16,5 @@ suite('Unit Tests', function(){
 //none requiered
 
 });
+
+*/
