d6b51d92383f49656c3157788dc317df399603f8
Brian Marquis <rbmarquis.dev@gmail.com>
2019-03-06 22:19:21 -0600

    wtf

--- routes/api.js
+++ routes/api.js
@@ -11,21 +11,88 @@ require('dotenv').config(); // comment-out on Glitch
 const axios = require('axios');
 const expect = require('chai').expect;
 const IEX_API_BASE_URL = "https://api.iextrading.com/1.0/stock/"
-// const mongo = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 //const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
+// mongo.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
+//   let db = client.db();
+//   err ? console.log('Database error: ' + err) : console.log('Successful database connection');
+// });
 
-module.exports = function (app, db) {
+
+module.exports = function (app) {
 
   app.route('/api/stock-prices').get(function (req, res) {
     const q = req.query;
     q.ip = req.ip;
     console.log(q);
+    
 
-    function processData(data) {
-      res.send(data);
-    }
+    function handleData(data) {
+      //console.log(data);
+      const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+      client.connect(function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected correctly to MongoDB server");
+
+        const db = client.db(); 
+        const coll = db.collection('likedstocks');
+
+        async function processData(data) {
+
+          const arr = data.map(async ele => {
+
+            const response = await coll.findOne({stock: ele.stock})
+              .then(doc => {
+
+                if (!q.like) {
+                  if (doc) {
+                    return {stock: doc.stock, price: ele.price, likes: doc.likes};
+                  } else {
+                    return {stock: ele.stock, price: ele.price, likes: 0};
+                  }
+                } else { // 'liked' stock
+                  if (!doc) {
+                    let stock = ele.stock, likes = 1, ips = [req.ip];
+                    let newDoc = {stock: ele.stock, likes: 1, ips: [req.ip]};
+                    async function insertDoc(doc) {
+                      const response =  await coll.insertOne(doc).catch(error => console.log(error));
+                      return response;
+                    }
+                    insertDoc(newDoc)
+                    .then(() => ({stock, price: ele.price, likes}));
+                    
+                  }
+                }
+
+              })
+              .catch(error => console.log(error))
+            ;
+
+            return response;
+
+          });
+
+          const processedData = await Promise.all(arr);
+
+          return processedData;
+
+        }; // END processData
+
+
+
+        processData(data)
+          .then(result => console.log(result))
+          .then(client.close(false, () => console.log('database closed')));
+        
+      }); // END client.connect
+
+      res.send('done');
+
+    } // END handleData
 
     let datareq = typeof q.stock === 'string' 
       ? [axios.get(IEX_API_BASE_URL + q.stock + '/quote'), null] 
@@ -53,7 +120,7 @@ module.exports = function (app, db) {
         // handle error
         console.log(error);
       })
-      .then(data => processData(data))
+      .then(data => handleData(data))
     ;
     
   });
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const helmet = require('helmet');
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
-const mongo = require('mongodb').MongoClient;
+//const mongo = require('mongodb').MongoClient;
 const PORT = process.env.PORT || 3000; 
 const NODE_ENV = process.env.NODE_ENV;
 
@@ -23,47 +23,37 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongo.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
-  let db = client.db();
-  err ? console.log('Database error: ' + err) : console.log('Successful database connection');
-  
-  // Index page (static HTML)
-  app.route('/').get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-  // For FCC testing purposes
-  fccTestingRoutes(app);
-
-  // Routing for API 
-  apiRoutes(app, db);  
-      
-  // 404 Not Found Middleware
-  app.use(function(req, res, next) {
-    res.status(404).type('text').send('Not Found');
-  });
-
-  //Start our server and tests!
-  app.listen(PORT, function () {
-    console.log("Listening on port " + PORT);
-    if(NODE_ENV==='test') {
-      console.log('Running Tests...');
-      setTimeout(function () {
-        try {
-          runner.run();
-        } catch(e) {
-          let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-        }
-      }, 3500);
-    }
-  });
-  
-
-
+// Index page (static HTML)
+app.route('/').get(function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// For FCC testing purposes
+fccTestingRoutes(app);
 
+// Routing for API 
+apiRoutes(app);  
+    
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404).type('text').send('Not Found');
+});
+
+//Start our server and tests!
+app.listen(PORT, function () {
+  console.log("Listening on port " + PORT);
+  if(NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        let error = e;
+        console.log('Tests are not valid:');
+        console.log(error);
+      }
+    }, 3500);
+  }
+});
 
 module.exports = app; //for testing
