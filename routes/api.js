abc3f89077e74862faebf1b410f482b226aa58c8
Brian Marquis <rbmarquis.dev@gmail.com>
2019-03-14 19:40:06 -0500

    finish api

--- routes/api.js
+++ routes/api.js
@@ -8,123 +8,67 @@
 
 'use strict';
 require('dotenv').config(); // comment-out on Glitch
-const axios = require('axios');
+
 const expect = require('chai').expect;
-const IEX_API_BASE_URL = "https://api.iextrading.com/1.0/stock/"
-const MongoClient = require('mongodb').MongoClient;
+const iex = require('../utils/dataIEX');
+const mongo = require('../utils/dbMongo');
 const assert = require('assert');
-
-//const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
-// mongo.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
-//   let db = client.db();
-//   err ? console.log('Database error: ' + err) : console.log('Successful database connection');
-// });
-
 
 module.exports = function (app) {
 
-  app.route('/api/stock-prices').get(function (req, res) {
-    const q = req.query;
-    q.ip = req.ip;
-    console.log(q);
+  app.route('/api/stock-prices').get(async (req, res, next) => {
     
+    try {
 
-    function handleData(data) {
-      //console.log(data);
-      const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
+      const iexData = await iex.getStockAndPriceData(req.query.stock, next);
+      const client = await mongo.connect(); 
+      const db = client.db(); 
+      const coll = db.collection("likedstocks");
+      const isLiked = req.query.like;
+      
+      const stockData = await Promise.all(
 
-      client.connect(function (err, client) {
-        assert.equal(null, err);
-        console.log("Connected correctly to MongoDB server");
+        iexData.map(async (obj) => {
+          
+          const promise = await coll.findOne({stock: obj.stock})
+            .then(isFound => (
+              isLiked 
+                ? coll.findOneAndUpdate({stock: obj.stock}, { 
+                      $inc: {likes: (isFound && isFound.ips.indexOf(req.ip) >= 0 ? 0 : 1)},
+                      $addToSet: {ips: req.ip}
+                    }, 
+                    {upsert: true, returnOriginal: false})
+                    .then(result => {
+                      const {stock, likes} = result.value;
+                      return {stock, price: obj.price, likes};
+                    })
+                : isFound 
+                  ? {stock: isFound.stock, price: obj.price, likes: isFound.likes}
+                  : {stock: obj.stock, price: obj.price, likes: 0}
+            ));
 
-        const db = client.db(); 
-        const coll = db.collection('likedstocks');
+          return promise;
+        })
 
-        async function processData(data) {
+      ).then(dataArr => ({
+        stockData: dataArr.length === 1
+          ? dataArr[0] 
+          : (dataArr.map((obj, i, dataArr) => {
+              const {stock, price, likes} = obj;
+              return {stock, price, rel_likes: (likes - dataArr[(dataArr.length - 1 - i)].likes)}
+            }))
+      }));
 
-          const arr = data.map(async ele => {
+      mongo.close(client);
+      
+      res.send(stockData);
 
-            const response = await coll.findOne({stock: ele.stock})
-              .then(doc => {
+    } catch (err) {
+      console.log('Error:', err.message);
+      next(err);
+    }
 
-                if (!q.like) {
-                  if (doc) {
-                    return {stock: doc.stock, price: ele.price, likes: doc.likes};
-                  } else {
-                    return {stock: ele.stock, price: ele.price, likes: 0};
-                  }
-                } else { // 'liked' stock
-                  if (!doc) {
-                    let stock = ele.stock, likes = 1, ips = [req.ip];
-                    let newDoc = {stock: ele.stock, likes: 1, ips: [req.ip]};
-                    async function insertDoc(doc) {
-                      const response =  await coll.insertOne(doc).catch(error => console.log(error));
-                      return response;
-                    }
-                    insertDoc(newDoc)
-                    .then(() => ({stock, price: ele.price, likes}));
-                    
-                  }
-                }
-
-              })
-              .catch(error => console.log(error))
-            ;
-
-            return response;
-
-          });
-
-          const processedData = await Promise.all(arr);
-
-          return processedData;
-
-        }; // END processData
-
-
-
-        processData(data)
-          .then(result => console.log(result))
-          .then(client.close(false, () => console.log('database closed')));
-        
-      }); // END client.connect
-
-      res.send('done');
-
-    } // END handleData
-
-    let datareq = typeof q.stock === 'string' 
-      ? [axios.get(IEX_API_BASE_URL + q.stock + '/quote'), null] 
-      : [axios.get(IEX_API_BASE_URL + q.stock[0] + '/quote'), 
-        axios.get(IEX_API_BASE_URL + q.stock[1] + '/quote')]
-    ;
-
-    axios.all(datareq)
-      .then(axios.spread(function (res1, res2) {
-        let data = [], resArr = [res1, res2];
-        
-        for (let i = 0; i < resArr.length; ++i) {
-          if (resArr[i]) {
-            data.push({
-              stock: resArr[i].data.symbol, 
-              price: resArr[i].data.latestPrice.toString()
-            });
-          }
-        }
-
-        return data;
-
-      }))
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(data => handleData(data))
-    ;
-    
   });
-  
     
 };
 
